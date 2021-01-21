cc8420341df8775ebf432af2a2c43e63e5896115
TylwythTeg <garciarobertoiv@yahoo.com>
2021-01-21 03:18:37 -0800

    I added engage state to the Footer which required raising selectedValue state from Radio up to the Footer

--- client/src/components/options/content/radio/index.js
+++ client/src/components/options/content/radio/index.js
@@ -7,78 +7,53 @@ class Radio extends Component {
         // ARG: props.options is a list of objects: 
         //            Obj Attributes are .html (jsx html)  .value (str defining value)
         // ARG (Optional): props.title is a string for the title. Not passed, no HTML for it generated
-        // ARG (Optional): props.defaultValue is the default value. Defaults to first in list (props.allowDeselect is false) or no selection (props.allowDeselect is true)
         // ARG (Optional): props.allowDeselect. Defaults to false (true means clicking a selected item will deselect it)
         // ARG (Optional): props.baseClassName is class string for any classname you want to pass. Default is "radio"
         // ARG (Optional): props.itemClassName is class string for any style you want to pass FOR THE RADIO OPTION DIVS. Default is "radio_options"
+        // ARG: onUpdate: is a function that receives 1 arg of 'newSelection' with .value attribute
+        // ARG: selectedValue: a value to be selected, must be in among values in options objects. For no selection pass nothing and make sure props.allowDeselect is true
 
-        const {options, allowDeselect} = props;
-        // optional
-        const defaultValue = props.defaultvalue;
-        const baseClassName = props.baseClassName;
-        const itemClassName = props.itemClassName;
-        const title = props.title;
+        const {selectedValue, options, title=null, defaultValue=null, baseClassName=null, itemClassName=null} = props;
 
-        if (defaultValue && !options.find(option => option.value === defaultValue)) {
-            throw new Error("Default value passed that is not found in the Options list");
+        if (selectedValue && !options.find(option => option.value === defaultValue)) {
+            throw new TypeError("props.defaultValue does not match any props.options item value");
         }
         if (baseClassName && typeof baseClassName !== 'string') {
-            throw new Error("props.baseClassName is not a string");
+            throw new TypeError("props.baseClassName is not a string");
         }
         if (itemClassName && typeof itemClassName !== 'string') {
-            throw new Error("props.itemClassName is not a string");
+            throw new TypeError("props.itemClassName is not a string");
         }
         if (title && typeof title !== 'string') {
-            throw new Error("props.title is not a string");
+            throw new TypeError("props.title is not a string");
         }
-
-        this.state = {
-                selectedValue: defaultValue || (allowDeselect ? null : options[0].value),
-        }; 
 
         this.handleClick = this.handleClick.bind(this);
     }
 
     handleClick(event) {
         const newSelection = this.props.options.find(option => option.value === event.currentTarget.dataset.value);
-        
-        if (this.props.allowDeselect) {
-            // Need to compare previous selectedValue to newSelection
-            // Deselect if same, make clicked option the selection if not
-            this.setState((state, props) => ({
-                selectedValue: (state.selectedValue === newSelection.value) ? null : newSelection.value,
-            }));
 
-        } else {
-            // just make clicked option the selection
-            this.setState({
-                selectedValue: newSelection.value,
-            });
-        }
-        
-        
+        this.props.onUpdate(newSelection.value);
     }
 
 
     render() {
-        const title = this.props.title ? <span>{this.props.title}</span> : null; 
+        const {selectedValue, options, baseClassName="radio", itemClassName="radio_options"} = this.props;
+        const title = this.props.title ? <span>{this.props.title}</span> : null;
+        
 
-        const baseClassName = this.props.baseClassName || "radio";
-        const itemClassName = this.props.itemClassName || "radio_options";
-
-        const options = [];
-        this.props.options.forEach(opt => {
+        const choices = options.map( option => {
             //TODO: For accessibility, this should be an input/radio setup rather than the div it is now
-            //                         It would alter our use of data-value
-            const className = itemClassName + (this.state.selectedValue === opt.value ? " selected" : "" );
-            options.push(<div onClick={this.handleClick} data-value={opt.value} className={className}>{opt.html}</div>);
+            //                         It would alter our use of data-value and have CSS implications
+            const className = itemClassName + (selectedValue === option.value ? " selected" : "" );
+            return <div onClick={this.handleClick} data-value={option.value} className={className}>{option.html}</div>;
         });
-
 
         return (
             <div className={baseClassName}>
                 {title}
-                {options}
+                {choices}
             </div>
         );
     }
--- client/src/components/options/footer/index.js
+++ client/src/components/options/footer/index.js
@@ -7,27 +7,55 @@ class Footer extends Component {
 
   constructor(props) {
       super(props);
+
+      const {engaged=true} = props;
+
+      this.state = {
+        enegaged: engaged,
+        selectedValue: null,
+      }
+
+      this.onUpdate = this.onUpdate.bind(this);
+  }
+
+  onUpdate(newValue) {
+      // Compare previous selectedValue to newSelection
+      // Deselect and unengage if same, make clicked option the selection and set to engaged if not
+      this.setState((state, props) => {
+        const sameValue = (this.state.selectedValue === newValue);
+
+        return {
+          selectedValue: sameValue ? null : newValue,
+          engaged: sameValue ? false : true,
+        }
+      });
   }
 
     render() {
+      const containerClass = "visual_mod_bar";
+      const itemClass = "option";
+      console.log(this.state.engaged);
+      const baseClassName = this.state.engaged ? containerClass + " engaged" : containerClass;
+
       const options = [
         {
-            html: <><button type="button"><i class="fas fa-chess-rook"></i></button><label>Chords</label></>,
+            html: <><button type="button"><i className="fas fa-chess-rook"></i></button><label>Chords</label></>,
             value: "Chords"
         },
         {
-            html: <><button type="button"><i class="fas fa-chess-bishop"></i></button><label>Scales</label></>,
+            html: <><button type="button"><i className="fas fa-chess-bishop"></i></button><label>Scales</label></>,
             value: "Scales"
         },
         {
-            html: <><button type="button"><i class="fas fa-cog"></i></button><label>Settings</label></>,
+            html: <><button type="button"><i className="fas fa-cog"></i></button><label>Settings</label></>,
             value: "Settings"
         }
     ];
 
 
+
     return (
-        <Radio baseClassName="visual_mod_bar" itemClassName="option" options={options} allowDeselect={true} ></Radio>
+        <Radio onUpdate={this.onUpdate} selectedValue={this.state.selectedValue} baseClassName={baseClassName} itemClassName={itemClass} options={options} allowDeselect={true} ></Radio>
     );
   }
 }
