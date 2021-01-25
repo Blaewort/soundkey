68f1fd9774d522cfa3524ee0816ac47bbe313713
TylwythTeg <garciarobertoiv@yahoo.com>
2021-01-25 03:46:55 -0800

    set up a starting state in options

--- client/src/scenes/visualizer/index.js
+++ client/src/scenes/visualizer/index.js
@@ -4,24 +4,23 @@ import Content from '../../components/content/index';
 import GuitarFretboard from './guitarFretboard/index';
 import {useParams} from 'react-router-dom';
 import { fetchChord } from '../../services/api/index';
-import { Chord } from 'chord-expressions';
+//import { Chord } from 'chord-expressions';
 import PianoFretboard from './pianoFretboard/index';
 
 
 
 function Visualizer(props) {
+    return null;
+    return null;
+    /*
     let notation = useParams().notation;
     let instrument = useParams().instrument
-    let chord = Chord.chordFromNotation(notation);
+    //let chord = Chord.chordFromNotation(notation);
     let notes = [];
     chord.notes.forEach(note =>{
         notes.push(note.name);
     });
-    /*
-    useEffect(() => {
-        fetchChord(notation.notation);
-    });
-    */
+
     let instrumentVisualizer;
     if(instrument === "guitar"){
         instrumentVisualizer = <GuitarFretboard tuningNotes={["E","B","G","D","A","E"]} selectedNotes={ notes }/>;
@@ -29,13 +28,6 @@ function Visualizer(props) {
         instrumentVisualizer = <PianoFretboard tuningNotes={["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"]} selectedNotes={ notes }></PianoFretboard>
     }
     console.log(notes);
-    /* Not needed, keeping as example code
-    let modeMenu;
-    if(mode === "chord"){
-        modeMenu = <Chord></Chord>
-    } else if (mode === "scale") {
-        modeMenu = <Scale></Scale>
-    } */
 
 
     return (
@@ -45,7 +37,7 @@ function Visualizer(props) {
                 {instrumentVisualizer}
             </Content>
         </div>
-    );
+    );*/
   }
   
   export default Visualizer;
